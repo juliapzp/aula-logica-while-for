0e872e1d9e38b51cd9c34379c999ea1326411a08
juliapzp <julyanap36@gmail.com>
2023-08-04 08:40:57 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+ function executaWhile(){
+    let num = (document.getElementById("entrada").value)
+    let i = 0;
+    while(i < num){
+        alert(i);
+        i ++;
+    }
+ }
+ function executaFor(){
+    let num = (document.getElementById("entrada").value)
+    for(let i = 0; i < num; i++){
+        alert(i);
+    }
+ }
